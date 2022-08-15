b38a5b4416dabf330536625536756efb39a5773f
PilarCiganda <cigandapilar@gmail.com>
2022-08-15 15:46:43 -0300

    filtro gris

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const modal = document.getElementById("modalID");
 const boton = document.getElementById("botonID");
 const span = document.getElementsByClassName("close") [0];
+const pantallaGris = document.getElementById("app")
 
 // Si el usuario toca el botón se abre ventana:
 
 boton.onclick = function() {
     modal.style.display = "block";
+    pantallaGris.style.filter =  "grayscale(100%)";
+
 }
 
 // Si el usuario toca x, cierra la ventana
 
 span.onclick = function() {
     modal.style.display = "none";
+    pantallaGris.style.filter =  "none";
 }
 
 
